76a75a18dd71f0af5b8d6ac068276db7eb4565d5
alikewmk <wang@networking.io>
2012-06-15 12:58:02 +0800

    init

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+# -*- encoding : utf-8 -*-
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+
+require 'bundler/capistrano'
+
+set :rvm_path, "/usr/local/rvm"
+set :rvm_bin_path, "/usr/local/rvm/bin"
+set :rvm_trust_rvmrcs_flag, 1
+set :normalize_asset_timestamps, false
+
+set :application, "magic_orders"
+
+set :use_sudo, false
+set :scm, :git
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :keep_releases, 3
+
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+
+role :web, "magicbeta.doorder.com" # Your HTTP server, Apache/etc
+role :app, "magicbeta.doorder.com" # This may be the same as your `Web` server
+role :db, "magicbeta.doorder.com", :primary => true # This is where Rails migrations will run
+
+set :user, "root"
+set :repository, "git@github.com:nioteam/magic_orders.git"
+set :branch, "master"
+set :deploy_to, "/var/rails/magic_orders"
+
+# tasks
+namespace :deploy do
+
+  desc "Restart web server"
+  task :restart, roles: :app, except: {no_release: true} do
+    run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+
+  desc "Symlink shared resources on each release"
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/system #{release_path}/public/system"
+  end
+end
+
+after 'deploy:finalize_update', 'deploy:symlink_shared'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicOrders::Application.config.secret_token = '775d0c3fcef2b0838aaf70d264ea7fc2b48f82a33fbbfff92e73c98c1d5f803368c8b26200089e5d35e14b2a0dc93b935d88f896ef436ee7bc3d848b318ba0ef'
