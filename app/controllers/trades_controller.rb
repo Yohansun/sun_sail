02234c85d30cb59bb4abfaca8ddae7280cd6c7c2
alikewmk <wang@networking.io>
2012-08-02 12:49:01 +0800

    [#MagicOrder] 申请开票功能修改 @120802

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -117,9 +117,13 @@ class TradesController < ApplicationController
       @trade.invoice_name = params[:invoice_name].strip
     end
 
-    unless params[:invoice_date].blank?
-      @trade.invoice_date = params[:invoice_date].strip
+    unless params[:invoice_content].blank?
+      @trade.invoice_content = params[:invoice_content].strip
     end
+
+    # unless params[:invoice_date].blank?
+    #   @trade.invoice_date = params[:invoice_date].strip
+    # end
 
     if params[:seller_confirm_deliver_at] == true
       @trade.seller_confirm_deliver_at = Time.now
