69f8101213e4e9d1c7d622b1871e1ef5f2f42d73
shawnyu <yu.chaong@gmail.com>
2012-08-31 18:02:25 +0800

    测试并修复bugs

--- app/workers/trade_taobao_memo_fetcher.rb
+++ app/workers/trade_taobao_memo_fetcher.rb
@@ -3,21 +3,18 @@ class TradeTaobaoMemoFetcher
 	include Sidekiq::Worker
   sidekiq_options :queue => :taobao_memo_fetcher
 
-  def perform(id)
-    trade = TaobaoTrade.find(id)
-    return unless trade
-    
-    TaobaoFu.select_source(trade.trade_source_id)
+  def perform(tid, trade_source_id)
+    TaobaoFu.select_source(trade_source_id)
 
     response = TaobaoFu.get(
     	method: 'taobao.trade.get',
       fields: 'buyer_message',
-      tid: trade.tid
+      tid: tid
     )
 
     return unless response && response["trade_get_response"]
     remote_trade = response["trade_get_response"]["trade"]
 
-    trade.update_attributes(buyer_message: remote_trade['buyer_message'], seller_memo: remote_trade['seller_memo'])
+    TaobaoTrade.where(tid: tid).update_all(buyer_message: remote_trade['buyer_message'])
   end
 end
