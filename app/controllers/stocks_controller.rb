a7db5fd822aab6d71fe96f1e9b2958da6ac145c4
geekontheway <zxl51@qq.com>
2012-10-05 17:06:10 +0800

    仓库管理的搜索功能

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,8 +3,30 @@ class StocksController < ApplicationController
 	before_filter :check_stock_type, except: [:home]
 
   def index
-  	select_sql = "products.name, products.taobao_id, products.status, stock_products.*"
-  	@products = StockProduct.joins(:product).select(select_sql).where(seller_id: params[:seller_id]).page params[:page]
+  	select_sql = "products.name, products.iid, products.taobao_id, products.category_id, products.status, stock_products.*"
+  	@products = StockProduct.joins(:product).select(select_sql).where(seller_id: params[:seller_id])
+    
+    if params[:info_type].present? && params[:info].present?
+      @products = @products.where("products.#{params[:info_type]} like ?", "%#{params[:info].strip}%")
+    end
+    
+    if params[:category].present?
+      @products = @products.where("products.category_id = ?", params[:category])
+    end
+    
+    if params[:stock_state].present?
+      case params[:stock_state]
+      when 'safe' 
+        @products = @products.where("stock_products.activity < stock_products.safe_value")
+      when 'max'
+        @products = @products.where("stock_products.actual = stock_products.max ")
+      else 
+        @products = @products.where("stock_products.actual != stock_products.max AND stock_products.activity >= stock_products.safe_value")
+      end         
+    end
+    
+    @products = @products.page params[:page]
+    
   end
 
   def home
