496b143e606a2719ac361e6c37349bb2bd46fcad
hui <hui@networking.io>
2012-10-07 14:36:21 +0800

    修改权限组名称

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,21 +3,23 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
   belongs_to :resource, :polymorphic => true
-  
+
   scopify
 
   def role_s
     case name
     when 'admin'
-      '管理员'
+      '系统管理员'
     when 'seller'
       '经销商'
-	  when 'support'
-	  	'售后服务'
-	  when 'interface'
-	  	'销售部联络人'
-	  when 'stock_admin'
-	  	'仓库管理'
+    when 'cs'
+      '客服'
+    when 'cs_read'
+      '客服（只读权限）'
+    when 'interface'
+      '销售部联络人'
+    when 'stock_admin'
+      '仓库管理员'
     end
   end
 end
