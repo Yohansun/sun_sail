aa4e46f38fd99249b5a2abaf7c75bf7bef623c9e
shawnyu <yu.chaong@gmail.com>
2012-10-19 11:48:45 +0800

    不可拆单订单才自动分流

--- app/models/taobao_trade.rb
+++ app/models/taobao_trade.rb
@@ -167,10 +167,14 @@ class TaobaoTrade < Trade
   def matched_seller(area = nil)
     area ||= default_area
     if TradeSetting.company == 'dulux'
-      Dulux::SellerMatcher.match_trade_seller(self, area)
+      Dulux::SellerMatcher.match_trade_seller(self, area) if splitable?
     else
       super
     end
+  end
+
+  def splitable?
+    match_seller_with_conditions(self).size == 1
   end
 
   def dispatchable?
