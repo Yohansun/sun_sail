a470eaacbf85491dfa69cb8a17b90664e8c9c560
Leslie Zhao <zxl51@qq.com>
2012-11-05 20:00:42 +0800

    分流条件判断

--- app/workers/delay_auto_dispatch.rb
+++ app/workers/delay_auto_dispatch.rb
@@ -7,7 +7,6 @@ class DelayAutoDispatch
   	trade = Trade.where(id: id).first
   	return unless trade
    	trade.auto_dispatch!
-   	trade.operation_logs.build(operated_at: Time.now, operation: '延时自动分流')
    	trade.save
   end
 end
