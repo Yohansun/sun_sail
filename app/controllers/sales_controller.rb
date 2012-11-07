55cbfe64f8681fad4898beb0d52c9b61ab05a027
alikewmk <wang@networking.io>
2012-11-08 01:13:44 +0800

    almost_perfect_timing X2

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -8,52 +8,57 @@ class SalesController < ApplicationController
     @sale = Sale.last
     TradeSetting.test_time = Time.now
     TradeSetting.test_time_2 = TradeSetting.test_time
+    TradeSetting.frequency = 900   ## per second
+    unless @sale
+      @sale = Sale.create(name: "测试活动", earn_guess: 100000, start_at: TradeSetting.test_time, end_at: TradeSetting.test_time + 1.day)
+    end
+    if TradeSetting.test_time.to_i < @sale.start_at.to_i
+      @data = [[TradeSetting.test_time, 0, 0]]
+    else
+      frequency = TradeSetting.frequency
+      if TradeSetting.test_time.to_i >= @sale.end_at.to_i
+        gap = @sale.end_at.to_i - @sale.start_at.to_i
+      else
+        gap = TradeSetting.test_time.to_i - @sale.start_at.to_i
+      end
+      @data = []
+      num = gap/frequency
+      if gap%frequency > 0
+        num += 1
+      end
 
-    # if Time.now < @sale.start_at
-      @data = [[Time.now, 0, 0]]
-    # elsif Time.now >= @sale.end_at
-    #   frequency = TradeSetting.gap_time               ## per second
-    #   gap = @sale.end_at.to_i - @sale.start_at.to_i
-    #   @data = []
-    #   num = gap/frequency
-    #   if gap%frequency > 0
-    #     num += 1
-    #   end
-    #   (0..num).each do |counter|
-    #     start_time = @sale.start_at + counter * frequency
-    #     if (counter + 1) * frequency > gap
-    #       end_time = @sale.end_at
-    #     else
-    #       end_time = @sale.start_at + (counter + 1) * frequency
-    #     end
-
-    #     all_in_frequency = @sale.all_trade_fee(start_time, end_time)
-    #     paid_in_frequency = @sale.paid_trade_fee(start_time, end_time)
-    #     amount_all = @data.size > 0 ? @data.last[1] + all_in_frequency : all_in_frequency
-    #     amount_paid = @data.size > 0 ? @data.last[2] + paid_in_frequency : paid_in_frequency
-
-    #     @data << [start_time, amount_all, amount_paid]
-    #   end
-    # else
-    # end
-
+      (0..num).each do |counter|
+        start_time = @sale.start_at + counter * frequency
+        if (counter + 1) * frequency > gap
+          end_time = @sale.end_at
+        else
+          end_time = @sale.start_at + (counter + 1) * frequency
+        end
+        all_in_frequency = @sale.all_trade_fee(start_time, end_time)
+        paid_in_frequency = @sale.paid_trade_fee(start_time, end_time)
+        amount_all = @data.size > 0 ? @data.last[1] + all_in_frequency : all_in_frequency
+        amount_paid = @data.size > 0 ? @data.last[2] + paid_in_frequency : paid_in_frequency
+        @data << [start_time, amount_all, amount_paid]
+        @progress_bar = (amount_paid/@sale.earn_guess*100).to_i
+      end
+    end
   end
 
   def add_node
-    #@sale = Sale.last
-    frequency = TradeSetting.gap_time
-    time_now = TradeSetting.test_time_2 #######trouble here
+    @sale = Sale.last
+    frequency = TradeSetting.frequency
+    time_now = TradeSetting.test_time_2
     start_time = time_now + frequency
     end_time = time_now + frequency * 2
     TradeSetting.test_time_2 += frequency
 
-    all_in_frequency = 0#@sale.all_trade_fee(start_time, end_time)
-    paid_in_frequency = 0#@sale.paid_trade_fee(start_time, end_time)
+    all_in_frequency = @sale.all_trade_fee(start_time, end_time)
+    paid_in_frequency = @sale.paid_trade_fee(start_time, end_time)
     amount_all = all_in_frequency
     amount_paid = paid_in_frequency
-
+    progress_bar = (amount_paid/@sale.earn_guess*100).to_i
     respond_to do |format|
-      format.json { render json: {year: start_time.year, month: start_time.month, day: start_time.day, hour: start_time.hour, min: start_time.min, amount: amount_all, amountall: amount_paid}}
+      format.json { render json: {year: start_time.year, month: start_time.month, day: start_time.day, hour: start_time.hour, min: start_time.min, amount: amount_all, amountall: amount_paid, progress_bar: progress_bar}}
     end
   end
 
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -19,7 +19,6 @@ class Sale < ActiveRecord::Base
 
   def all_trade_fee(start_time, end_time)
     all_trades = Trade.where("$and" => [{:created.gte => start_time.utc}, {:created.lte => end_time.utc}])
-    p all_trades.count
     all_money = all_trades.inject(0) { |sum, trade| sum + trade.calculate_fee }
     all_money.to_i
   end
@@ -27,7 +26,6 @@ class Sale < ActiveRecord::Base
   def paid_trade_fee(start_time, end_time)
     paid_trades = Trade.where("$and" => [{:created.gte => self.start_at.utc}, {:pay_time.gte => start_time.utc}, {:pay_time.lte => end_time.utc}])
     if paid_trades
-      p paid_trades.count
       paid_money = paid_trades.inject(0) { |sum, trade| sum + trade.calculate_fee }
       paid_money.to_i
     end
