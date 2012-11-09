388068539f3ea4dcf4707c858cba0955ab6d299d
Leslie Zhao <zxl51@qq.com>
2012-11-10 02:02:23 +0800

    rescue trade  _id

--- app/workers/delay_auto_dispatch.rb
+++ app/workers/delay_auto_dispatch.rb
@@ -4,7 +4,7 @@ class DelayAutoDispatch
   sidekiq_options :queue => :delay_auto_dispatch
 
   def perform(id)
-  	trade = Trade.where(id: id).first
+  	trade = Trade.where(_id: id).first
   	return unless trade
    	trade.auto_dispatch!
    	trade.save
