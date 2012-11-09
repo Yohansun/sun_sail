c5b19fc39b9bd7b0829e07631dcf5b6d319097fd
alikewmk <wang@networking.io>
2012-11-09 11:56:57 +0800

    订单金额聚合筛选改

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -17,13 +17,13 @@ class Sale < ActiveRecord::Base
   end
 
   def all_trade_fee(start_time, end_time)
-    all_trades = Trade.where("$and" => [{:created.gte => start_time.utc }, {:created.lte => end_time.utc}])
+    all_trades = Trade.where("$and" => [{:created.gte => start_time.utc + 8.hour }, {:created.lte => end_time.utc + 8.hour}])
     all_money = all_trades.inject(0) { |sum, trade| sum + trade.calculate_fee }
     all_money.to_i
   end
 
   def paid_trade_fee(start_time, end_time)
-    paid_trades = Trade.where("$and" => [{:created.gte => self.start_at.to_time }, {:pay_time.gte => start_time.utc }, {:pay_time.lte => end_time.utc }])
+    paid_trades = Trade.where("$and" => [{:created.gte => self.start_at.utc + 8.hour }, {:pay_time.gte => start_time.utc + 8.hour }, {:pay_time.lte => end_time.utc + 8.hour }])
     if paid_trades
       paid_money = paid_trades.inject(0) { |sum, trade| sum + trade.calculate_fee }
       paid_money.to_i
