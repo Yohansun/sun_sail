95cb0069f9971bee413921c2507b611f3d2eb873
shawnyu <yu.chaong@gmail.com>
2012-11-20 19:32:24 +0800

    修复order_by错误

--- app/controllers/trade_reports_controller.rb
+++ app/controllers/trade_reports_controller.rb
@@ -19,7 +19,7 @@ class TradeReportsController < ApplicationController
     else
     	@start_date = @end_date = @start_time = @end_time = ''
     end	
-  	reports = reports.order_by("performed_at DESC")
+  	reports = reports.order_by(:performed_at.desc)
 		@reports = reports.page params[:page]
 	end	
 
