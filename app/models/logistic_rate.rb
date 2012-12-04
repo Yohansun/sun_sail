7c27299c7e05044cb20852c70bc3b07bae432399
Leslie Zhao <zxl51@qq.com>
2012-12-04 15:22:11 +0800

    抓取11月订单评分

--- app/models/logistic_rate.rb
+++ app/models/logistic_rate.rb
@@ -1,16 +1,17 @@
 # -*- encoding : utf-8 -*-
 class LogisticRate < ActiveRecord::Base
 
-  attr_accessible  :logistic_id, :mobile, :send_at, :seller_id, :tid, :score
+  attr_accessible  :logistic_id, :mobile, :send_at, :seller_id, :tid, :score, :taobao_rate_score
   
   def self.rates_array(seller_id, start_date, end_date, logistic_id)
   	rates = LogisticRate.where(seller_id: seller_id, logistic_id: logistic_id).where(:send_at => start_date..end_date)
   	arr = Array.new(8,0)
   	if rates
-	  	arr[0] = rates.count #订单数 
-	  	arr[1] = arr[0] * 5 #物流总分 
-	  	arr[2] = rates.sum(:taobao_rate_score) #物流评分 
-	  	arr[3] = (arr[2].to_f / arr[1]).round(4) * 100 #物流评分满意度 
+	  	arr[0] = rates.count #订单数
+	  	taobao_rates = rates.where("taobao_rate_score in (1,3,5)") 
+	  	arr[1] = taobao_rates.count * 5 #订单总分 
+	  	arr[2] = rates.sum(:taobao_rate_score) #订单评分 
+	  	arr[3] = ((arr[2].to_f / arr[1]) * 100).round(2) #物流评分满意度 
 	  	valid_rates = rates.where("score in (1,3,5)")
 	  	if valid_rates
 	  		arr[4] = valid_rates.count #有效回复数
@@ -19,7 +20,7 @@ class LogisticRate < ActiveRecord::Base
 	  			arr[6] = valid_rates.sum(:score) #短信评分
 	  		end
 	  		if arr[5] != 0
-	  			arr[7] = (arr[6].to_f / arr[5]).round(4) * 100 #短信评分满意度
+	  			arr[7] = ((arr[6].to_f / arr[5])* 100).round(2) #短信评分满意度
 	  		end
 	  	end	
 	  end	
