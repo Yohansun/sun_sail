0813aff7ccfb6bf3f5e25bcce4441be25f956d5b
shawn <yu.chaong@gmail.com>
2012-12-11 17:21:06 +0800

    修改首页入口项

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+	def stock_path_by_user
+		if current_user.has_role?(:seller) && current_user.seller 
+			if TradeSetting.company != 'nippon'
+				seller_stocks_path(current_user.seller.id)
+			end
+		elsif current_user.has_role?(:admin)
+			stocks_path
+		elsif current_user.has_role?(:cs)
+			stock_products_path
+		else
+			'#'
+		end 
+	end
 end
