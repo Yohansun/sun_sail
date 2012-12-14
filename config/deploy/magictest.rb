f7cc9f9462d83b56b13af7f4714aa4cbecc3024e
hui <hui@networking.io>
2012-12-14 12:01:31 +0800

    调整cap部署到不同的分支

--- config/deploy/magictest.rb
+++ config/deploy/magictest.rb
@@ -3,7 +3,7 @@ set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :system
 
 set :repository, "git@github.com:nioteam/magic_orders.git"
-set :branch, "master"
+set :branch, "test"
 
 server "magictest.networking.io", :web, :app, :db, primary: true
 set :user, "root"
