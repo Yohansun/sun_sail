8ca7fdc4b2f5c7247b857ae873b72ddc97b5eb78
Raecoo Cao <raecoo@gmail.com>
2013-01-08 11:20:32 +0800

    updates the ReconcileStatementDetail default value

--- db/migrate/20130105055631_create_reconcile_statement_details.rb
+++ db/migrate/20130105055631_create_reconcile_statement_details.rb
@@ -2,18 +2,18 @@ class CreateReconcileStatementDetails < ActiveRecord::Migration
   def change
     create_table :reconcile_statement_details do |t|
       t.integer :reconcile_statement_id
-      t.integer :alipay_revenue
-      t.integer :postfee_revenue
-      t.integer :trade_success_refund
-      t.integer :sell_refund
-      t.integer :base_service_fee
-      t.integer :store_service_award
-      t.integer :staff_award
-      t.integer :taobao_cost
-      t.integer :audit_cost
-      t.integer :collecting_postfee
-      t.integer :audit_amount
-      t.integer :adjust_amount
+      t.integer :alipay_revenue, default: 0
+      t.integer :postfee_revenue, default: 0
+      t.integer :trade_success_refund, default: 0
+      t.integer :sell_refund, default: 0
+      t.integer :base_service_fee, default: 0
+      t.integer :store_service_award, default: 0
+      t.integer :staff_award, default: 0
+      t.integer :taobao_cost, default: 0
+      t.integer :audit_cost, default: 0
+      t.integer :collecting_postfee, default: 0
+      t.integer :audit_amount, default: 0
+      t.integer :adjust_amount, default: 0
       t.timestamps
     end
     add_index :reconcile_statement_details, :reconcile_statement_id
