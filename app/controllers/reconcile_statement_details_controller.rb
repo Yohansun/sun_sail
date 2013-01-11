55b07665fbdac2859928825cc64961e29312e3f4
alikewmk <wang@networking.io>
2013-01-11 13:05:04 +0800

    金额调整修复，财务管理添加权限设置

--- app/controllers/reconcile_statement_details_controller.rb
+++ app/controllers/reconcile_statement_details_controller.rb
@@ -1,15 +1,21 @@
 # -*- encoding : utf-8 -*-
 class ReconcileStatementDetailsController < ApplicationController
-	before_filter :authenticate_user!
-	before_filter :fetch_rsd, only: [:show]
-	def show
+  before_filter :authenticate_user!
+  before_filter :fetch_rsd, only: [:show]
+  before_filter :check_module
+  def show
     @trade_details = @rsd.select_trades(params[:page])
     @money_type = params[:money_type]
-	end
+  end
 
-	private
+  private
 
   def fetch_rsd
     @rsd = ReconcileStatementDetail.find(params[:id])
+  end
+
+  def check_module
+    redirect_to "/" if TradeSetting.enable_module_reconcile_statements != true
+    redirect_to "/" if !current_user.has_role?(:admin)
   end
 end
