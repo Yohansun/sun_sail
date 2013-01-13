b448f95ed26c4f69b579b87cd68688274217613a
hui <hui@networking.io>
2013-01-13 12:01:09 +0800

    去掉无法正常执行的migration

--- db/migrate/20130108045054_remove_bbs_topic_id_to_uplaod_files.rb
+++
@@ -1,9 +0,0 @@
-class RemoveBbsTopicIdToUplaodFiles < ActiveRecord::Migration
-  def up
-    remove_column :uplaod_files, :quantity
-  end
-
-  def down
-    add_column :uplaod_files, :quantity, :integer
-  end
-end
