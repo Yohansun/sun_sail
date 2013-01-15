7ce9cd7b6db797eb54e5042f3b2681bee833d1ab
hui <hui@networking.io>
2013-01-15 11:24:43 +0800

    修正CI系统中的数据初始化问题

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,7 @@ require 'omniauth/strategies/taobao'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # app name MagicOrders订单test
-  provider :taobao, TradeSetting.taobao_app_key, TradeSetting.taobao_app_secret
+  if ActiveRecord::Base.connection.tables.include?('settings') and !defined?(::Rake)
+    provider :taobao, TradeSetting.taobao_app_key, TradeSetting.taobao_app_secret
+  end
 end
