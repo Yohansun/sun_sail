0320cf5659a767c265fa5a19689aae973c703608
geekontheway <zxl51@qq.com>
2013-01-27 16:01:04 +0800

    fix test

--- db/migrate/20130116061845_add_bbs_topic_id_to_upload_files.rb
+++ db/migrate/20130116061845_add_bbs_topic_id_to_upload_files.rb
@@ -1,5 +1,5 @@
 class AddBbsTopicIdToUploadFiles < ActiveRecord::Migration
   def change
-  	# add_column :upload_files, :bbs_topic_id, :integer
+  	add_column :upload_files, :bbs_topic_id, :integer
   end
 end
