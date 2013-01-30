1f9bb4a88a80266989105f0679108801128d2c3e
alikewmk <wang@networking.io>
2013-01-30 19:39:49 +0800

    物流商信息初始化

--- app/models/logistic.rb
+++ app/models/logistic.rb
@@ -22,4 +22,34 @@ class Logistic < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_presence_of :code
+
+  def self.three_mostly_used
+
+    map = %Q{
+      function() {
+        if(this.logistic_id != null){
+        emit(this.logistic_id, {num: 1});
+        }
+      }
+    }
+
+    reduce = %Q{
+      function(key, values) {
+        var result = {num: 0};
+        values.forEach(function(value) {
+          result.num += value.num;
+        });
+        return result;
+      }
+    }
+
+    map_reduce_info = Trade.between(created: (Time.now - 3.months)..Time.now).ne(logistic_id: nil).map_reduce(map, reduce).out(inline: true).sort{|a, b| b['value']['num'] <=> a['value']['num']}
+    ids = [].tap do |id|
+      map_reduce_info.each_with_index do |info, i|
+        id << info["_id"].to_i if i <= 2
+      end
+    end
+    ids
+  end
+
 end
