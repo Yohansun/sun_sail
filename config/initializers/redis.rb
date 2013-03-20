4177aaba8e964d57b4bc383eb327614bec797f2e
geekontheway <zxl51@qq.com>
2013-03-20 15:38:41 +0800

    fix redis name space

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,4 @@
-# Load the redis.yml configuration file
-unless Rails.env.test?
-  redis_config = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env]
-  # Connect to Redis using the redis_config host and port
-  if redis_config
-    $redis = Redis::Namespace.new(redis_config['namespace'])
-  end
-end
+require "redis"
+require "redis-namespace"
+redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
+$redis = Redis::Namespace.new(redis_config['namespace'], :redis => $redis)
