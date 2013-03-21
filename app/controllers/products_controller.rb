ffe8647b17a8ddd940f6c8ccb39547cb197b39ee
geekontheway <zxl51@qq.com>
2013-03-21 16:42:57 +0800

    隐藏商品管理中的未完成功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,12 +38,6 @@ class ProductsController < ApplicationController
   def create
     @product = current_account.products.new params[:product]
     if @product.save
-      if params[:skus] #THIS IS TEMPORARY
-        params[:skus]["sku_ids"].each do |s|
-          Sku.create(properties_name: "1:1:" + s, product_id: @product.id, sku_id: 12345678, num_iid: 12345678, properties: "1:1", quantity: 1) unless s == ''
-        end
-      end
-
       if params[:product]['good_type'] == '2' && params[:child_iid].present?
         @product.create_packages(params[:child_iid])
       end
@@ -64,12 +58,6 @@ class ProductsController < ApplicationController
       @product.packages.delete_all
       @product.create_packages(params[:child_iid])
     end
-
-    # if params[:skus] #THIS IS TEMPORARY
-    #   params[:skus]["sku_ids"].each do |s|
-    #     Sku.create(properties_name: "1:1:" + s, product_id: @product.id, sku_id: 12345678, num_iid: 12345678, properties: "1:1", quantity: 1) unless s == ''
-    #   end
-    # end
 
     if @product.update_attributes(params[:product])
       redirect_to products_path
