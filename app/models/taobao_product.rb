12eee0a7454cf810e227231712004729a8110e80
alikewmk <wang@networking.io>
2013-05-09 15:22:40 +0800

    修改淘宝商品是否已绑定本地SKU列

--- app/models/taobao_product.rb
+++ app/models/taobao_product.rb
@@ -7,4 +7,14 @@ class TaobaoProduct < ActiveRecord::Base
 
   attr_accessible :name, :product_id, :outer_id, :price, :pic_url, :category_id, :cat_name, :detail_url, :num_iid, :cid, :account_id,:sku_id,:taobao_skus_attributes
   # add validation
+
+  def has_bindings
+    taobao_skus.each do |sku|
+      if sku.sku_bindings.present? && sku.sku_bindings != []
+        return "已绑定"
+        break
+      end
+    end
+    return "未绑定"
+  end
 end
