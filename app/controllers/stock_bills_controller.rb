2d03682328434f2b310a9d05d6e704c06ebfb33a
Zhoubin <zhoubin@networking.io>
2013-05-29 13:05:49 +0800

    TASK #1102 magic_search 搜索模式 _nu bug

--- app/controllers/stock_bills_controller.rb
+++ app/controllers/stock_bills_controller.rb
@@ -4,8 +4,7 @@ class StockBillsController < ApplicationController
   before_filter :fetch_bills
   
   def index
-    params[:search] ||= {}
-    params[:search][:_id_in] = params[:export_ids].split(',') if params[:export_ids].present?
+    parse_params
     @search = @bills.search(params[:search])
     @bills = @search.page(params[:page]).per(20)
     @count = @search.count
@@ -18,5 +17,17 @@ class StockBillsController < ApplicationController
       @bills = StockBill.where(account_id: current_account.id, :stocked_at.ne => nil)
     end
   end
-
+  
+  def parse_params
+    search = params[:search] ||= {}
+    params[:search][:_id_in] = params[:export_ids].split(',') if params[:export_ids].present?
+    if params[:bill_products_sku_id_eq].present?
+      search[:bill_products_sku_id_eq] = params[:bill_products_sku_id_eq]
+    end
+    if params[:checked_at] == "nil"
+      search[:checked_at_not_eq] = nil
+    elsif params[:checked_at] == "true"
+      search[:checked_at_eq] = nil
+    end
+  end
 end
--- lib/plugins/magic_search/lib/magic_search.rb
+++ lib/plugins/magic_search/lib/magic_search.rb
@@ -1,5 +1,6 @@
 require 'magic_search/search'
 module MagicSearch
+  TRUE = ["true",true,1,"1"]
   
   DEFALUT_WHERE = {
     "_like"   => {:to => "$regex"     ,:alias => "_contains" ,:convert => proc {|v| %r/#{v}/ } },    
@@ -8,8 +9,8 @@ module MagicSearch
     "_gte"    => {:to => "$gte"       ,:alias => "_greater_than_or_equal" },
     "_lt"     => {:to => "$lt"        ,:alias => "_less_than"             },
     "_lte"    => {:to => "$lte"       ,:alias => "_less_than_or_equal"    },
-    "_not_eq" => {:to => "$ne"        ,:alias => "_does_not_equal"        },
-    "_in"     => {:to => "$in"        ,:alias => "_in"          ,:convert => proc {|v| v.is_a?(Array) ? v : v.to_s.split(',') }                    },
-    "_nu"     => {:to => "$exists"    ,:alias => "_is_not_null" ,:convert => proc {|v| !["false",0,"0","",false,nil].include?(v) } }
+    "_not_eq" => {:to => "$ne"        ,:alias => "_does_not_equal",:convert => proc {|v| TRUE.include?(v) ? "" : v }        },
+    "_in"     => {:to => "$in"        ,:alias => "_in"            ,:convert => proc {|v| v.is_a?(Array) ? v : v.to_s.split(',') }                    },
+    "_nu"     => {:to => "$exists"    ,:alias => "_is_not_null"   ,:convert => proc {|v| TRUE.include?(v.to_s) } }
   }
 end
