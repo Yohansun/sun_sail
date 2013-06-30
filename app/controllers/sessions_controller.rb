6e681040ea49522b3709c89b16aceb286d1227b8
ZhouBin <zhoubin@networking.io>
2013-06-30 22:54:48 +0800

    [TASK#1473] 登陆成功去掉notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    # set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
     current_account = self.resource.accounts.first
     session[:account_id] = current_account.id
