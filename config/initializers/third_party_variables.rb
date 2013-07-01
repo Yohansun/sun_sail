0e64024346691204fe25b52d11165288dc0d07a9
leslie <xiaoliang@networking.io>
2013-07-01 11:49:58 +0800

    BUG #415 标杆正式接口的地址不正确

--- config/initializers/third_party_variables.rb
+++ config/initializers/third_party_variables.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  $biaogan_client = "http://58.210.118.230:9012/order/BMLservices"
+  $biaogan_client = "http://58.210.118.230:9012/order/BMLservices/BMLQuery?wsdl"
   $biaogan_customer_id = "AY_BLS"
   $biaogan_customer_password= "BML39390"
 else
