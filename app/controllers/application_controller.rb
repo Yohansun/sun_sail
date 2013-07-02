7ef735979094b354a6ffbe90d1b455ebe6ad1639
yohansun <yohansun@ubuntu.ubuntu-domain>
2013-07-02 13:18:49 +0800

    bug_425   异常中心提示有36个异常，刷新或者点进去没有异常

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,14 +32,14 @@ class ApplicationController < ActionController::Base
   end
 
   def alert_count
-    @count = 0
+    @alert_counts = 0
     return false if current_user.nil?
     if current_user.allow_read?(:trades,:seller) && current_user.seller
       trades = Trade.where(account_id: current_account.id, seller_id: current_user.seller.id)
     else
       trades = Trade.where(account_id: current_account.id)
     end
-    @count = trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "buyer_delay_deliver", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count +
+    @alert_counts = trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "buyer_delay_deliver", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count +
             trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "seller_ignore_deliver", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count +
             trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "seller_lack_product", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count +
             trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "seller_lack_color", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count +
@@ -49,19 +49,19 @@ class ApplicationController < ActionController::Base
     if current_account.settings.auto_settings["auto_mark_unusual_trade"]
       unusual_auto_settings = current_account.settings.auto_settings["unusual_conditions"]
       if unusual_auto_settings['unusual_waitpay']
-        @count += trades.where(:unusual_states.elem_match => {key: "unpaid_in_#{unusual_auto_settings['max_unpaid_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
+        @alert_counts += trades.where(:unusual_states.elem_match => {key: "unpaid_in_#{unusual_auto_settings['max_unpaid_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
       end
       if unusual_auto_settings['unusual_deliver']
-        @count += trades.where(:unusual_states.elem_match => {key: "undelivered_in_#{unusual_auto_settings['max_undelivered_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
+        @alert_counts += trades.where(:unusual_states.elem_match => {key: "undelivered_in_#{unusual_auto_settings['max_undelivered_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
       end
       if unusual_auto_settings['unusual_dispatch']
-        @count += trades.where(:unusual_states.elem_match => {key: "undispatched_in_#{unusual_auto_settings['max_undispatched_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
+        @alert_counts += trades.where(:unusual_states.elem_match => {key: "undispatched_in_#{unusual_auto_settings['max_undispatched_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
       end
       if unusual_auto_settings['unusual_receive']
-        @count += trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "unreceived_in_#{unusual_auto_settings['max_unreceived_days']}_days", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count
+        @alert_counts += trades.where("$and" => [{"unusual_states" => {"$elemMatch" => {key: "unreceived_in_#{unusual_auto_settings['max_unreceived_days']}_days", repaired_at: nil}}}, {"$or" => [{:has_buyer_message.ne => true}, {:buyer_message.ne => nil}]}]).count
       end
       if unusual_auto_settings['unusual_repair']
-        @count += trades.where(:unusual_states.elem_match => {key: "unrepaired_in_#{unusual_auto_settings['max_unrepaired_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
+        @alert_counts += trades.where(:unusual_states.elem_match => {key: "unrepaired_in_#{unusual_auto_settings['max_unrepaired_days']}_days", :repaired_at => nil}).or({:has_buyer_message.ne => true}, {:buyer_message.ne => nil}).count
       end
     end
   end
