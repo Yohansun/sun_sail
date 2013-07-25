d2e6d0619c04fa67e4cbc18b4eaddbd15820289d
leslie <xiaoliang@networking.io>
2013-07-25 13:12:34 +0800

    BUG #467 已经存在的店铺 重新授权后 不应该走一键开始里面的 MagicOneHitFetcher

--- app/workers/magic_one_hit_fetcher.rb
+++ app/workers/magic_one_hit_fetcher.rb
@@ -7,17 +7,20 @@ class MagicOneHitFetcher
   def perform(account_id)
     # more settings here
     account = Account.find_by_id(account_id)
-    trade_sources = account.trade_sources
-    return if trade_sources.blank?
-    trade_sources.each do |trade_source| 
-    TaobaoTradePuller.create(Time.now - 3.month, Time.now, trade_source.id)
-      TaobaoLogisticsOrdersPuller.create(Time.now - 3.month, Time.now, trade_source.id)
-      TaobaoProductsPuller.create_from_trades!(trade_source.id)
-      TaobaoProductsPuller.sync_cat_name(trade_source.id)
-      base_url = TradeSetting.base_url
-      HTTParty.put("#{base_url}/account_setups/#{account_id}/data_fetch_finish")
-    end  
-    CustomerFetch.perform_async
-    account.settings.init_data_ready = true
-  end  
+    if !Trade.where(account_id: account_id).exists? && !Product.where(account_id: account_id).exists?
+      trade_sources = account.trade_sources
+      return if trade_sources.blank?
+      trade_sources.each do |trade_source|
+        unless
+        TaobaoTradePuller.create(Time.now - 3.month, Time.now, trade_source.id)
+        TaobaoLogisticsOrdersPuller.create(Time.now - 3.month, Time.now, trade_source.id)
+        TaobaoProductsPuller.create_from_trades!(trade_source.id)
+        TaobaoProductsPuller.sync_cat_name(trade_source.id)
+        base_url = TradeSetting.base_url
+        HTTParty.put("#{base_url}/account_setups/#{account_id}/data_fetch_finish")
+      end
+      CustomerFetch.perform_async
+      account.settings.init_data_ready = true
+    end
+  end
 end
