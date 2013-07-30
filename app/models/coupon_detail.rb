6feeb1b4a3827b278c9bdbbbb502b4bc083dd1a9
wang <wang@networking.io>
2013-07-30 18:50:37 +0800

    coupon_detail的order_id添加别名

--- app/models/coupon_detail.rb
+++ app/models/coupon_detail.rb
@@ -4,7 +4,7 @@ class CouponDetail
 
   field :coupon_price,        type: Float
   field :coupon_type,         type: String
-  field :order_id,            type: String
+  field :order_id,  as: :oid, type: String
   field :sku_id,              type: String
 
   embedded_in :jingdong_trades
