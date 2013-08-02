741f7cf040cfb14c2a58b28d4751b76af463e05b
ZhouBin <zhoubin@networking.io>
2013-08-02 17:44:35 +0800

    AddResourceIdToSkuBindings bug

--- db/migrate/20130801110505_add_resource_id_to_sku_bindings.rb
+++ db/migrate/20130801110505_add_resource_id_to_sku_bindings.rb
@@ -1,7 +1,7 @@
 class AddResourceIdToSkuBindings < ActiveRecord::Migration
   def change
     add_column :sku_bindings,:resource_id,:integer
-    add_column :sku_bindings,:resource_type,:integer
+    add_column :sku_bindings,:resource_type,:string
     SkuBinding.update_all("resource_id = taobao_sku_id, resource_type = 'TaobaoSku'")
   end
 end
