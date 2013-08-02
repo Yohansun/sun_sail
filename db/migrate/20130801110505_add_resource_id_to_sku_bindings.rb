f8155c351ce1ba7c814cb69555a321f13d5e78d3
ZhouBin <zhoubin@networking.io>
2013-08-02 12:05:16 +0800

    先添加resource_id&resource_type 把taobao_sku_id的值存到resource_id,resource_type=TaobaoSku中再删除taobao_sku_id

--- db/migrate/20130801110505_add_resource_id_to_sku_bindings.rb
+++ db/migrate/20130801110505_add_resource_id_to_sku_bindings.rb
@@ -2,5 +2,6 @@ class AddResourceIdToSkuBindings < ActiveRecord::Migration
   def change
     add_column :sku_bindings,:resource_id,:integer
     add_column :sku_bindings,:resource_type,:integer
+    SkuBinding.update_all("resource_id = taobao_sku_id, resource_type = 'TaobaoSku'")
   end
 end
