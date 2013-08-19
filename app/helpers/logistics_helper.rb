d23c79ec9bf53a6ea84bb4a6361c8bfd13b8141b
ZhouBin <zhoubin@networking.io>
2013-08-19 14:59:52 +0800

    获取物流商ID优化

--- app/helpers/logistics_helper.rb
+++ app/helpers/logistics_helper.rb
@@ -9,32 +9,44 @@ module LogisticsHelper
 
     source_name = source_type.underscore.gsub(/trade$/,'source')
     source_id = current_account.send(source_name).try(:id)
-
-    cache(:expires_in => 1.hour).fetch("#{source_type}_#{source_id}_#{name}") do
+    cache_name = "#{source_type}:#{source_id}"
+    cache(:expires_in => 1.hour).fetch("#{cache_name}_#{name}}") do
       case source_type
-      when "TaobaoTrade"    then get_taobao_logistic(name).id rescue nil
-      when "YihaodianTrade" then get_yihaodian_logistic(name).id rescue nil
-      when "JingdongTrade"  then get_jingdong_logistic(name).logistics_id rescue nil
+      when "TaobaoTrade"    then get_taobao_logistic(name,cache_name).id rescue nil
+      when "YihaodianTrade" then get_yihaodian_logistic(name,cache_name).id rescue nil
+      when "JingdongTrade"  then get_jingdong_logistic(name,cache_name).logistics_id rescue nil
       end
     end
   end
 
-  def get_taobao_logistic(name)
-    response = TaobaoQuery.get({method: "taobao.logistics.companies.get", fields: 'id,code,name,reg_mail_no'},current_account.taobao_source.id)
+  private
+  def get_taobao_logistic(name,cache_name=nil)
+    response = cache(:expires_in => 1.hour).fetch(cache_name+"_taobao") do
+      TaobaoQuery.get({method: "taobao.logistics.companies.get", fields: 'id,code,name,reg_mail_no'},current_account.taobao_source.id)
+    end
+
     logistics_company = response["logistics_companies_get_response"]["logistics_companies"]["logistics_company"]
     Hashie::Mash.new logistics_company.find {|u| u["name"] =~ /^#{name.to(1)}}/}
   end
 
-  def get_yihaodian_logistic(name)
+  def get_yihaodian_logistic(name,cache_name=nil)
     api_paramters = current_account.yihaodian_query_conditions
-    response = YihaodianQuery.post({method: 'yhd.logistics.deliverys.company.get'},api_paramters)
+
+    response = cache(:expires_in => 1.hour).fetch(cache_name+"_yihaodian") do
+      YihaodianQuery.post({method: 'yhd.logistics.deliverys.company.get'},api_paramters)
+    end
+
     logistics_info = response["response"]["logisticsInfoList"]["logisticsInfo"]
     Hashie::Mash.new logistics_info.find {|u| u["companyName"] =~ /^#{name.to(1)}}/}
   end
 
-  def get_jingdong_logistic(name)
+  def get_jingdong_logistic(name,cache_name=nil)
     api_paramters = current_account.jingdong_query_conditions
-    response = JingdongQuery.get({method: '360buy.delivery.logistics.get'},api_paramters)
+
+    response = cache(:expires_in => 1.hour).fetch(cache_name+"_jingdong") do
+      JingdongQuery.get({method: '360buy.delivery.logistics.get'},api_paramters)
+    end
+
     logistics_list = response["delivery_logistics_get_response"]["logistics_companies"]["logistics_list"]
     Hashie::Mash.new logistics_list.find {|u| u["logistics_name"] =~ /^#{name.to(1)}}/}
   end
