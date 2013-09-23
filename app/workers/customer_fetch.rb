584fcd1780c0532ce55e211e63556e672b013037
wang <wang@networking.io>
2013-09-24 00:18:06 +0800

    worker custom_fetch add retry false

--- app/workers/customer_fetch.rb
+++ app/workers/customer_fetch.rb
@@ -1,8 +1,8 @@
 #encoding: utf-8
 class CustomerFetch
   include Sidekiq::Worker
-  sidekiq_options :queue => :customer_fetch
-  
+  sidekiq_options :queue => :customer_fetch, :retry => false
+
   def perform(account_id=nil,ec_name=nil)
     if account_id && ec_name
       CustomersPuller.send("#{ec_name}_sync",account_id)
