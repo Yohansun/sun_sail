6169bfb61364c41971f281cd8f961cdb2e93f4a8
wang <wang@networking.io>
2013-11-05 10:21:20 +0800

    BUG #880 替换 帐号 为 账号

--- app/workers/init_user_notifier.rb
+++ app/workers/init_user_notifier.rb
@@ -6,7 +6,7 @@ class InitUserNotifier
   def perform(account_id, email, password, mobiles)
     account = Account.find(account_id)
     if mobiles
-      sms = Sms.new(account, "Magic系统初始化用户提醒, 帐号: #{email}, 密码: #{password},请登录后尽快修改密码", mobiles)
+      sms = Sms.new(account, "Magic系统初始化用户提醒, 账号: #{email}, 密码: #{password},请登录后尽快修改密码", mobiles)
       sms.transmit
     end
     if email
