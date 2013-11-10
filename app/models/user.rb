c2fcdc3a7b830ce422f8d57d5d87443065c48b29
ZhouBin <zhoubin@networking.io>
2013-11-11 07:43:22 +0800

    BUG #901 用户名验证导致以前所有设置过的大部分失效

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,8 +71,8 @@ class User < ActiveRecord::Base
   validates_presence_of :password, on: :create
   validates :username ,
             uniqueness: {message: "该用户名已有人使用"},
-            presence: {message: "填写4-9位数字或者字母"},
-            format: { with: /\A[A-Za-z0-9]{4,9}\z/, message: "填写4-9位数字或者字母"}
+            presence: true
+#            format: { with: /\A[A-Za-z0-9]{4,9}\z/, message: "填写4-9位数字或者字母"}
 
   validate :has_phone_or_email?
 
