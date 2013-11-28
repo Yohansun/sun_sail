79e3b8fbaf5331918ab5928a85b721f282d85c02
ZhouBin <zhoubin@networking.io>
2013-11-28 18:17:43 +0800

    BUG ~>  TASK #2861 支持一个account关联同一平台的多个trade source
    对象被修改

--- app/controllers/logistics_controller.rb
+++ app/controllers/logistics_controller.rb
@@ -146,7 +146,7 @@ class LogisticsController < ApplicationController
     # 鉴于这一块如果做好的话得取同一平台下所有店铺物流商的集, 逻辑有点不清晰,而且代码量比较多, 而且这么做下来未必就是最好的解决方案.
     # 最好的就是本地物流信息绑定第三方物流商,如果第三方平台中的某个店铺更新了物流信息,在本地发货的时候没有找到, 或物流商信息变更了,
     # 导致发货失败,提示更新第三方物流,并手动绑定后在发货.
-    method = source_type.to_s << "_sources"
+    method = source_type.to_s.dup << "_sources"
     trade_source = current_account.respond_to?(method) && current_account.send(method).first
     @logistics.each do |l|
       tmp << {
