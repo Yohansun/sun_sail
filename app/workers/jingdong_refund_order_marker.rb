d09380d57909d05fc3f73f3f135e558458317407
ZhouBin <zhoubin@networking.io>
2013-12-06 18:23:18 +0800

    TASK #3011 京东退货信息同步队列, 报错消息更新

--- app/workers/jingdong_refund_order_marker.rb
+++ app/workers/jingdong_refund_order_marker.rb
@@ -9,24 +9,14 @@ class JingdongRefundOrderMarker
     trade_source = TradeSource.where(trade_type: "Jingdong",account_id: account_id).first
     query_conditions = trade_source.jingdong_query_conditions
     #没有加query_fields,需要加
-    response = JingdongQuery.get({method: '360buy.after.search',
-                                  select_fields: 'receive_state,return_item_list',
-                                  page: 100,
-                                  page_size: 10}, query_conditions)
-    if response['after_search_response']
-      return_infos = response['after_search_response']['after']["return_infos"]
-      if return_infos.present?
-        return_orders = return_infos['return_item_list']
-        if return_orders
-          return_orders.each do |order|
-            trade = JingdongTrade.where(tid: order['order_id']).first
-            if trade
-              return_order = trade.orders.where(sku_id: order['order_id']).first
-              return_order.refund_status = order['return_type']
-              return_order.save
-            end
-          end
-        end
+    data = {parameters: {method: '360buy.after.search',select_fields: 'receive_state,return_item_list',page: 100,page_size: 10}}
+    response = JingdongQuery.get(data[:parameters], query_conditions)
+    cache_exception!("退货信息抓取异常",data.merge({response: response,trade_source_id: trade_source_id})) do
+      return_orders = response['after_search_response']['after']["return_infos"]['return_item_list'].each do |order|
+        trade = JingdongTrade.where(tid: order['order_id']).first
+        return_order = trade.orders.where(sku_id: order['order_id']).first
+        return_order.refund_status = order['return_type']
+        return_order.save
       end
     end
   end
