e78e45ce1a3f4ce1b9365e1d0f06d8fc5f10acb0
ZhouBin <zhoubin@networking.io>
2013-12-10 18:30:44 +0800

    TASK #1957 【单店白兰氏】每日系统自动发送电商数据邮件需求补充

--- app/mailers/daily_orders_notifier.rb
+++ app/mailers/daily_orders_notifier.rb
@@ -29,28 +29,27 @@ class DailyOrdersNotifier < ActionMailer::Base
 
   def yesterday(account_id)
     @account = Account.find(account_id)
-    reciever = @account.settings.email_dailyorders_yesterday_reciever
-    cc = @account.settings.email_dailyorders_yesterday_cc
-    bcc = @account.settings.email_dailyorders_yesterday_bcc
-    from = @account.settings.email_notifier_from
 
     yesterday = 1.days.ago
-    yesterday_begin = yesterday.beginning_of_day
-    yesterday_end =  yesterday.end_of_day
-    
-    @taobao_trades = TaobaoTrade.where(account_id: account_id).between(:created => yesterday_begin..yesterday_end).count
-    @taobao_paid_trades = TaobaoTrade.where(account_id: account_id).between(pay_time: yesterday_begin..yesterday_end).count
-    @taobao_paid = TaobaoTrade.where(account_id: account_id).between(pay_time: yesterday_begin..yesterday_end).inject(0.0) { |sum, trade| sum + trade.payment }
+    yesterday_begin,yesterday_end = yesterday.beginning_of_day,yesterday.end_of_day
+    con = Trade.where({account_id: account_id}).between(:created => yesterday_begin..yesterday_end).between(pay_time: yesterday_begin..yesterday_end)
+    created_con,paid_con = con.except("pay_time"), con.except("created")
+    @taobao_trades = TaobaoTrade.where(paid_con)
+    @taobao_paid_trades = TaobaoTrade.where(created_con)
 
     if @account.key == "nippon"
-      @taobao_purchase_orders =  TaobaoPurchaseOrder.where(account_id: account_id).between(created: yesterday_begin..yesterday_end).count
-      @taobao_purchase_paid_orders =  TaobaoPurchaseOrder.where(account_id: account_id).between(pay_time: yesterday_begin..yesterday_end)
-      @taobao_purchase_paid = TradeDecorator.decorate(@taobao_purchase_paid_orders).inject(0.0) { |sum, trade| sum + trade.total_fee.to_f }
+      @taobao_purchase_orders =  TaobaoPurchaseOrder.where(created_con)
+      @taobao_purchase_paid_orders =  TaobaoPurchaseOrder.where(paid_con)
+      @taobao_purchase_paid_price = TradeDecorator.decorate(@taobao_purchase_paid_orders).inject(0.0) { |sum, trade| sum + trade.total_fee.to_f }
     end
 
-    email_subject = "#{yesterday.strftime("%Y年%m月%d日")} #{@account.settings.site_title_basic} 电商数据"
-
-    mail(:from => from, :to => reciever, :cc => cc, :bcc => bcc, :subject => email_subject)
+    mail({
+      :from     => @account.settings.email_notifier_from, 
+      :to       => @account.settings.email_dailyorders_yesterday_reciever, 
+      :cc       => @account.settings.email_dailyorders_yesterday_cc, 
+      :bcc      => @account.settings.email_dailyorders_yesterday_bcc, 
+      :subject  => "#{I18n.l(yesterday,format: :short)} #{@account.settings.site_title_basic} 电商数据"
+    })
   end
   private
   # subject '白兰氏','异常核查报告','2013-07-09 10:10:24'
