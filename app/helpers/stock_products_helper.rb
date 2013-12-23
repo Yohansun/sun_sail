adeabd7b408af5cae2c3fcd5e2a28a7afe7d8d85
wang <wang@networking.io>
2013-12-23 16:02:42 +0800

    TASK #3127 【Magic Order】窗帘客户 仓库管理-库存查询-搜索 增加“库存状态”筛选项，标红预警状态-1218

--- app/helpers/stock_products_helper.rb
+++ app/helpers/stock_products_helper.rb
@@ -1,10 +1,16 @@
+#encoding: utf-8
 module StockProductsHelper
-  def sku_option(id)
-    str = ''
-    skus = Sku.where(product_id: id)
-    skus.each do |sku|
-      str += "<option value='#{sku.id}'>#{sku.name}</option>"
-    end  
-    str 
+  def get_col_style(visible_cols, col_name, stock_product)
+    col_style = ""
+
+    if col_name == "storage_status" && stock_product.send(col_name) == "预警"
+      col_style += 'color:red;'
+    end
+
+    if !visible_cols.include?(col_name)
+      col_style += 'display:none'
+    end
+
+    col_style
   end
 end
