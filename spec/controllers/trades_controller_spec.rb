4d2ee207990b7dd6c9c40f766b0ab44c2d1dc1d9
wang <wang@networking.io>
2014-03-20 12:17:12 +0800

    单店测试：Trade相关测试--搭架子

--- spec/controllers/trades_controller_spec.rb
+++ spec/controllers/trades_controller_spec.rb
@@ -2,4 +2,97 @@ require 'spec_helper'
 
 describe TradesController do
 
+##### 显示所有订单 #####
+  describe "GET #index" do
+  end
+
+##### 显示订单相关操作 #####
+  describe "GET #show" do
+  end
+
+##### 显示订单预处理页面 #####
+  describe "GET #edit" do
+  end
+
+##### 更新订单 #####
+  describe "POST #update" do
+  end
+
+##### 导出报表 #####
+  describe "GET #export" do
+  end
+
+##### 批量确认发货 #####
+  describe "GET #batch_check_goods" do
+  end
+
+##### 导出报表操作－按搜索条件导出 #####
+  describe "GET #export" do
+  end
+
+##### 导出报表操作－按勾选订单导出 #####
+  describe "GET #batch_export" do
+  end
+
+##### 批量添加赠品 #####
+  describe "GET #batch_add_gift" do
+  end
+
+##### 验证批量添加赠品 #####
+  describe "GET #verify_add_gift" do
+  end
+
+##### 设置交易完成 #####
+  describe "GET #trade_finished" do
+  end
+
+##### 锁定订单 #####
+  describe "GET #lock_trade" do
+  end
+
+##### 激活已锁定订单 #####
+  describe "GET #activate_trade" do
+  end
+
+##### 淘宝匹配经销商 #####
+  describe "GET #seller_for_area" do
+  end
+
+##### 其他来源订单匹配经销商 #####
+  describe "GET #estimate_dispatch" do
+  end
+
+##### 批量发货 #####
+  describe "GET #batch_deliver" do
+  end
+
+## 话说这个玩意应该放到users_controller里吧？！
+##### 设置给用户分派订单的比例 #####
+  describe "GET #assign_percent" do
+  end
+
+##### 设置自动开发票 #####
+  describe "GET #change_invoice_setting" do
+  end
+
+##### 订单合并 #####
+  describe "GET #merge" do
+  end
+
+##### 合并订单拆分 #####
+  describe "GET #split" do
+  end
+
+##### 分拣单搜索 #####
+  describe "GET #sort_product_search" do
+  end
+
+##### 属性备注匹配入库单 #####
+  describe "GET #match_icp_bills" do
+  end
+
+##### 订单预处理－处理异常 #####
+  describe "GET #solve_unusual_state" do
+  end
+
 end
--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -3,21 +3,89 @@ require 'spec_helper'
 
 describe Trade do
   let(:current_account) { create(:account) }
-  let(:trade) { Fabricate(:trade, account_id: current_account.id) }
+  let(:taobao_trade) { Fabricate(:taobao_trade, account_id: current_account.id) }
 
   before do
     current_account
-    trade
+    taobao_trade
   end
 
-  context "#fetch_account" do
-    it "fetch the right account of trade" do
-      trade.fetch_account.id.should equal(current_account.id)
-      trade.orders.count.should equal(4)
+##### 状态更新测试 #####
+  context "trade update" do
+
+    it "should assign has_cs_memo" do
+      taobao_trade.update_attributes(cs_memo: "test_memo")
+      taobao_trade.has_cs_memo.should be_true
     end
-    pending "do nothing when trade has no account_id"
+
+    it "should assign has_refund_orders" do
+      taobao_trade.orders.first.update_attributes(refund_status: "SUCCESS")
+      taobao_trade.has_refund_orders.should be_true
+    end
+
+    it "should assign has_unusual_state" do
+      taobao_trade.unusual_states.create(reason: "买家延迟发货")
+      taobao_trade.has_unusual_state.should be_true
+    end
+
+    it "should assign has_property_memos" do
+      taobao_trade.trade_property_memos.create()
+      taobao_trade.has_property_memos.should be_true
+    end
   end
 
-  context "#trade_source" do
+##### 订单删除测试 #####
+  context "trade destroy" do
+    it "should delete all deliver_bills belongs to the trade" do
+      taobao_trade.generate_deliver_bill
+      trade_id = taobao_trade.id
+      taobao_trade.destroy
+      DeliverBill.where(trade_id: trade_id).count.should eq(0)
+    end
   end
+
+##### 操作人设置测试 #####
+  context "#set_operator" do
+  end
+
+##### 匹配经销商测试 #####
+  context "#matched_seller_with_default" do
+  end
+
+##### 发货单生成测试 #####
+  context "#generate_deliver_bill" do
+  end
+
+##### 物流单生成测试 #####
+  context "#generate_stock_out_bill" do
+  end
+
+##### 手动分流测试 #####
+  context "#dispatch!" do
+  end
+
+##### 自动分流测试 #####
+  context "#auto_dispatch!" do
+  end
+
+##### 分流重置测试 #####
+  context "#reset_seller" do
+  end
+
+##### 匹配物流商测试 #####
+  context "#matched_logistics" do
+  end
+
+##### 手动发货测试 #####
+  context "#deliver!" do
+  end
+
+##### 自动发货测试 #####
+  context "#auto_deliver!" do
+  end
+
+##### 搜索测试 #####
+  context "#filter" do
+  end
+
 end
